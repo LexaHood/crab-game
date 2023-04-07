702e526817b87357bf614d23eb80e0fdfed3b082
LexaHood <allpetr@list.ru>
2023-04-07 22:46:11 +0300

    Типы для клешней

--- src/store.ts
+++ src/store.ts
@@ -3,5 +3,21 @@ import { signal } from "@preact/signals";
 export type TDimensions = { x: number, y: number };
 export type TScreens = "start" | "game";
 
+export type TClawsCords = {
+  x: number,
+  y: number
+};
+
+export type TClaws = {
+  left: {
+    id: number | undefined,
+    clawCords: TClawsCords | undefined
+  },
+  right: {
+    id: number | undefined,
+    clawCords: TClawsCords | undefined
+  }
+};
+
 export const currentScreen = signal<TScreens>("start");
 export const appDimensions = signal<TDimensions | undefined>(undefined);
